07ec355a92911c6ceb5f756404bd31929c6961e8
nimairoy <nimairoy718@gmail.com>
2023-05-14 23:09:38 +0600

    emazon server created

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require('cors')
+require('dotenv').config();
+
+
+
+// middleware 
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res)=> {
+    res.send('Server is Running');
+})
+
+
+
+
+app.listen(port, ()=>{
+    console.log('Server is Running on PORT ', port);
+})
